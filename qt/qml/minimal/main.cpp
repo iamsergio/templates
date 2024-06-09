f17a091dfbab52c7553eea0c6341733b9691c517
Sergio Martins <@>
2024-06-09 15:44:21 +0100

    feat: Add minimal qml

---
+++ qt/qml/minimal/main.cpp
@@ -0,0 +1,15 @@
+#include <QtQuick>
+
+
+int main(int argc, char **argv)
+{
+    QGuiApplication app(argc, argv);
+
+    QQuickView view;
+    view.setSource(QUrl("qrc:/main.qml"));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.resize(500, 500);
+    view.show();
+
+    app.exec();
+}
