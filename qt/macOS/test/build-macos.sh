6f23ee878055011ccf96ad0d4eff87173109a67f
Sergio Martins <@>
2024-07-06 12:53:00 +0100

    chore: Add codesign

--- qt/macOS/test/build-macos.sh
+++ qt/macOS/test/build-macos.sh
@@ -8,6 +8,7 @@ mkdir build-macos-release-xcode &> /dev/null
 cd build-macos-release && \
 qt-cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=installed .. && \
 ninja -j10 && ninja install && \
+macdeployqt installed/test.app/ -qmldir=../ -codesign=$APPLE_DEVELOPMENT_IDENTITY && \
 cd ../build-macos-release-xcode && \
 qt-cmake -G "Xcode" .. && \
 xcodebuild -scheme test -configuration Release
