14c48b208c635f0ff3e24f8b2943c90dedbdf05d
Sergio Martins <@>
2024-07-06 12:31:27 +0100

    chore: minor fixes

--- qt/macOS/test/build-macos.sh
+++ qt/macOS/test/build-macos.sh
@@ -6,7 +6,7 @@ mkdir build-macos-release &> /dev/null
 mkdir build-macos-release-xcode &> /dev/null
 
 cd build-macos-release && \
-qt-cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=installed -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$APPLE_DEVELOPMENT_IDENTITY .. && \
+qt-cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=installed .. && \
 ninja -j10 && ninja install && \
 cd ../build-macos-release-xcode && \
 qt-cmake -G "Xcode" .. && \
