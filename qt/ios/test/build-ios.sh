dc0acef9f98a38e7d662935dd1cab22fe2ea37ca
Sergio Martins <@>
2024-07-07 19:23:03 +0100

    chore: Fixes

--- qt/ios/test/build-ios.sh
+++ qt/ios/test/build-ios.sh
@@ -6,9 +6,15 @@ unset SDKROOT
 mkdir build-debug-ios-xcode &> /dev/null
 
 cd build-debug-ios-xcode && \
-qt-cmake -G Xcode .. && \
+qt-cmake -G Xcode -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$APPLE_TEAM_ID .. && \
 xcodebuild -allowProvisioningUpdates -scheme test -configuration Debug -destination "name=iPhone 15" -destination-timeout 1 ENABLE_ONLY_ACTIVE_RESOURCES=NO
 
+cd .. && cd build-release-ios-xcode && \
+qt-cmake -G Xcode -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM=$APPLE_TEAM_ID .. && \
+xcodebuild -allowProvisioningUpdates -scheme test -configuration Release -destination "generic/platform=iOS" -destination-timeout 1 ENABLE_ONLY_ACTIVE_RESOURCES=NO
+
+
+# For simulator:
 # xcodebuild -allowProvisioningUpdates -scheme test -configuration Debug -destination "generic/platform=iOS" -destination-timeout 1 ENABLE_ONLY_ACTIVE_RESOURCES=NO
 
 # Use: -destination "id=<foo>" for a specific device
--- qt/ios/test/test.pro
+++ qt/ios/test/test.pro
@@ -6,5 +6,12 @@ RESOURCES += resources.qrc
 
 QT += quick
 
-# Not working with Qt, something about QtPDF
+# Arm64 simulator not supported by Qt yet (as of 6.7)
 # QMAKE_APPLE_SIMULATOR_ARCHS=arm64
+
+development_team.name = DEVELOPMENT_TEAM
+development_team.value = $$MY_DEV_TEAM # Pass via cmd line
+QMAKE_MAC_XCODE_SETTINGS += development_team
+
+QMAKE_TARGET_BUNDLE_PREFIX = com.sergiomartins.
+QMAKE_BUNDLE = test
