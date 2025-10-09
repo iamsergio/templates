80bc5152127f59b908b8b0a01484d376cb6c9f87
Sergio Martins <@>
2025-10-09 12:20:17 +0100

    up

---
+++ cpp/crash_second_thread/run_all.sh
@@ -0,0 +1,19 @@
+./crash_thread_exceptionsfp     
+sleep 1
+./crash_thread_no_exceptions_async_unwind_tables_fp   
+sleep 1
+./crash_thread_no_exceptions_fp  
+sleep 1
+./crash_thread_no_exceptions_sync_and_async_unwind_tables_fp    
+sleep 1
+./crash_thread_no_exceptions_unwind_tables_fp   
+sleep 1
+./crash_thread_exceptionsno-fp  
+sleep 1
+./crash_thread_no_exceptions_async_unwind_tables_no-fp
+sleep 1
+./crash_thread_no_exceptions_no-fp 
+sleep 1
+./crash_thread_no_exceptions_sync_and_async_unwind_tables_no-fp  
+sleep 1
+./crash_thread_no_exceptions_unwind_tables_no-fp
